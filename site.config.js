be14f602d752ee936173b41147a513d7a0697df1
varun7244 <72227999+varun7244@users.noreply.github.com>
2023-05-22 20:27:05 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://commons.wikimedia.org/wiki/File:Cat03.jpg", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["Blog", "Website", "Notion"],
   },
