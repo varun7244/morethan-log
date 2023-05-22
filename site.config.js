ed463451322f73380243efe48b3511fcf3cd1e97
varun7244 <72227999+varun7244@users.noreply.github.com>
2023-05-22 16:48:08 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.2b59ff92e4f248cc80f00970a02c1227,
+    pageId: process.env.NOTION_PAGE_ID,
   },
 
   // plugin configuration (optional)
